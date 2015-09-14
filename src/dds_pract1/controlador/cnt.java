0be2008ddd7b57d3c4c2d7efd02637233a718297
asistare7.alumnes <asistare7.alumnes@ub052054.mat.ub.edu>
2015-09-14 11:55:28 +0200

    Corregint un parell de coses, segon intent

---
+++ src/dds_pract1/controlador/cnt.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dds_pract1.controlador;
+
+/**
+ *
+ * @author asistare7.alumnes
+ */
+public class cnt {
+    
+}
