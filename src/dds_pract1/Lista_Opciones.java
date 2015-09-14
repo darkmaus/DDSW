275bee6f0001cf4a33a69a41129745591dc184ff
aleivaca9.alumnes <aleivaca9.alumnes@ub052056.mat.ub.edu>
2015-09-14 11:19:25 +0200

    Added lista opciones

---
+++ src/dds_pract1/Lista_Opciones.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dds_pract1;
+
+/**
+ *
+ * @author aleivaca9.alumnes
+ */
+public class Lista_Opciones {
+    String[] OpcionesPrincipales;
+    OpcionesPrincipales = new String[5];
+    
+    
+    String[] OpcionesClientes;
+    OpcionesClientes = new String[5];
+    
+    String[] OpcionesPisos;
+    OpcionesPisos = new String [5]
+
+    public String[] getOpcionesPrincipales() {
+        return OpcionesPrincipales;
+    }
+
+    public String[] getOpcionesClientes() {
+        return OpcionesClientes;
+    }
+
+    public String[] getOpcionesPisos() {
+        return OpcionesPisos;
+    }
+
+    public void setOpcionesPrincipales(String[] OpcionesPrincipales) {
+        this.OpcionesPrincipales = OpcionesPrincipales;
+    }
+
+    public void setOpcionesClientes(String[] OpcionesClientes) {
+        this.OpcionesClientes = OpcionesClientes;
+    }
+
+    public void setOpcionesPisos(String[] OpcionesPisos) {
+        this.OpcionesPisos = OpcionesPisos;
+    }
+    
+    
+}
