bf0f1c8a9c531f0f2114b006edc33716e0d93e04
mferrema35.alumnes <mferrema35.alumnes@ub052055.mat.ub.edu>
2015-09-14 10:47:32 +0200

    cliente

---
+++ src/dds_pract1/Cliente.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dds_pract1;
+
+/**
+ *
+ * @author mferrema35.alumnes
+ */
+public class Cliente {
+    int numerotel;
+    int DNI;
+    String nombre;
+    
+    public Cliente (int num, String name, int dni) {
+        numerotel = num;
+        nombre = name;
+        DNI = dni;
+    }
+    public void actualizarnombre(String name) {
+        nombre = name;
+    }
+    public void actualizardni(int dni) {
+        DNI = dni;
+    }
+    public void actualizartel(int num) {
+        numerotel = num;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getDNI() {
+        return DNI;
+    }
+    public int getNumero() {
+        return numerotel;
+    }
+}
