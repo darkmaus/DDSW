e76568e542804aff6cf9b9281394b838e72e69cc
aleivaca9.alumnes <aleivaca9.alumnes@ub052056.mat.ub.edu>
2015-09-14 11:32:27 +0200

    Added Opciones

--- src/dds_pract1/DDS_Pract1.java
+++ src/dds_pract1/DDS_Pract1.java
@@ -15,8 +15,29 @@ public class DDS_Pract1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //hola
+    int opcion;
+    opcion = 0;
+        System.out.println("Buenos días señor, bienvenido a BBB escoja la opcion deseada:");
+        
+        //Aqui empezamos a escoger la opción que quiera el empresario
+    switch(opcion){
+            case(1):
+            //Llamamos a la opción area clientes
+                //switch Editar/Registrar cliente
+            break;
+            case(2):
+            //llamamos la opción de registrar pisos
+                //switch Editar/Registrar pisos
+            break;
+            case(3):
+            //visualizamos lista de pisos para escoger el piso a visitar
+                 //switch lista de pisos
+            break;
+            case(4):
+            //cerramos la aplicación
+            break;
+        }
+        
     }
     
 }
